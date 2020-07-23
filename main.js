e15e1d689845890358a2c7bb5ae369dcf54c485b
yiguo-17 <yiguo@yiguo.localdomain>
2020-07-23 18:06:53 -0400

    add functions based on test file.

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
+const fs = require('fs');
+const studentInfo = JSON.parse(fs.readFileSync('..//students.json',{encoding:'utf-8'}));
+const studentsArr = studentInfo.students;
+const getStudentByIndex = function(num){
+  return studentsArr[num]
+}
 
+const getStudentByName = function(str){
+  for(const student of studentsArr){
+    if(student.name===str){return student;}
+  }
+  return -1;
+}
+
+const graduateStudent = function(str){
+  for(let i=0; i<studentsArr.length;i++){
+    if(studentsArr[i].name === str){
+      studentInfo.students[i].term++;
+      const newData = JSON.stringify(studentInfo);
+      console.log(newData);
+      fs.writeFileSync('../students.json',newData)
+      return 
+    }
+  }
+  return
+}
 
 
 
